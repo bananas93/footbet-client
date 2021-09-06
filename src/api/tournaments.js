277dc93e3c759b20881db25cea243548f19fa9a7
David <amerovdavid@gmail.com>
2021-09-06 21:31:42 +0300

    change function type

--- src/api/tournaments.js
+++ src/api/tournaments.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getCookie, logout } from '../helpers/authHelper';
 
-export function getTournaments() {
+export const getTournaments = () => {
   const token = getCookie('JWToken');
   return axios.get('https://footbet.herokuapp.com/api/tournaments', {
     headers: {
@@ -16,4 +16,4 @@ export function getTournaments() {
       }
       return res.data;
     }).catch((e) => e.response);
-}
+};
